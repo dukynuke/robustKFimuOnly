7e0d2473bee448936665f479a04e21ce4961b112
batucandan <84707606+dukynuke@users.noreply.github.com>
2021-05-24 12:39:17 +0300

    Add files via upload

---
+++ robustKF_STF.m
@@ -0,0 +1,49 @@
+% Robust Attitude Estimation via Single Tuning Factors (STFs) using IMU-Only Measurements 
+% Batu Candan and Halil Ersin Soken, 2021
+
+% Process Model
+F = eye(3) - dt*skew(wx,wy,wz) ;
+Q = -skew(x_c(1),x_c(2),x_c(3))*g_var*skew(x_c(1),x_c(2),x_c(3))*dt^2 ;
+% wx,wy,wz -> Gyroscope Measurements (rad/s).
+% skew is the skew-symmetric function transforming the vector to the matrix.
+% Q is the process noise covariance matrix.
+% g_var is the gyroscope noise variance.
+
+% Measurement Model
+H = norm(g)*eye(3) ; 
+R = a_var ;
+z = [ax;ay;az] - c_a*a_est ;
+% H is the measurement matrix.
+% g is the local gravity vector (m/s^2). 
+% R is the measurement noise covariance matrix.
+% ax,ay,az -> Accelerometer Measurements (m/s^2).
+% a_var is the accelerometer variance.
+% a_est is the estimation for external acceleration (m/s^2).
+% z is the measurement matrix.
+% c_a is a dimensionless, determinator constant specified for the cutoff  
+% frequency and the value of this constant is varying between 0 and 1.
+
+% Time Update (Prediction)
+x_p = F*x_c ;    % State Prediction
+P = F*P*F' + Q ; % Covariance Prediction
+e = z - H*x_p ;  % Innovation in KF
+
+% R-Adaptation via Novel Single Tuning Factor (STF) Method
+ek = e ;                                % Single 
+S = (ek'*ek-trace(H*P*H')-trace(R))/3 ; % Tuning
+S = max(S,0) ;                          % Factor Construction 
+
+% Measurement Update (Correction)
+K = P*H'*inv(H*P*H'+S*eye(3)+R) ;       % Kalman Gain Evaulation
+x_c = x_p + K*e ; x_c = x_c/norm(x_c) ; % State Correction and Normalization
+P = (eye(3)-K*H)*P ;                    % Covariance Correction
+
+% External Acc. Estimation (Gravity Compensation)
+a_est = [ax;ay;az] - norm(g)*x_c ;
+
+r_hat = atan2(x_c(2),x_c(3)) ;                   % Roll Estimate (rad)
+p_hat = atan2(-x_c(1),sqrt(x_c(2)^2+x_c(3)^2)) ; % Pitch Estimate (rad)
+
+ax_hat = a_est(1) ;        % 
+ay_hat = a_est(2) ;        % External Acc. Estimates (m/s^2)
+az_hat = a_est(3) - g(3) ; %
